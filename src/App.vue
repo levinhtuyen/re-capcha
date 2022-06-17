36f498264547ce102a1523e7d48e21358cc248c2
levinhtuyen <42595840+levinhtuyen@users.noreply.github.com>
2022-06-17 11:27:41 +0700

    create capcha

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template></template>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
